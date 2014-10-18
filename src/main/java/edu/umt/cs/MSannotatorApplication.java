a4a20bbc92159f9c4c9cf8c7645e93b78530a6c6
Benjamin Campbell <benjamin.campbell@mso.umt.edu>
2014-10-17 18:58:13 -0600

    Add Default Application Instance
    Maven generates quickstart application files that represent build and
    test files.

---
+++ src/main/java/edu/umt/cs/MSannotatorApplication.java
@@ -0,0 +1,13 @@
+package edu.umt.cs;
+
+/**
+ * Hello world!
+ *
+ */
+public class MSannotatorApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
